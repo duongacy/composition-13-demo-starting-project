f09de648fd51626e756e7daf9430e128e4b393f0
Lê Ý <duongy96@gmail.com>
2024-02-13 01:21:04 +0700

    update composition api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <user-list :users="activeUsers" @list-projects="selectUser"></user-list>
-    <projects-list :user="selectedUser"></projects-list>
+    <projects-list :user="selectedUser" v-if="selectedUser"></projects-list>
   </main>
 </template>
 
